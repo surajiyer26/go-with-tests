1ca1234fa67e3c8869f86d5a36bf163b47aba252
Suraj Iyer <iyersuraj7@gmail.com>
2025-11-08 12:28:08 +0530

    return Hello, World when a name is not provided

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const HelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return HelloPrefix + name
 }
 
 func main() {
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Suraj")
-	want := "Hello, Suraj"
+	t.Run("Saying hello to people, when name is provided", func(t *testing.T) {
+		got := Hello("Suraj")
+		want := "Hello, Suraj"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying hello to the world, when name is not provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
