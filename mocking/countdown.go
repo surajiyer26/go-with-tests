e67fb66575c02e3f905648c0d0d9b845849b51ec
Suraj Iyer <iyersuraj7@gmail.com>
2025-11-09 12:54:33 +0530

    replaced default sleeper with a configurable sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,10 +14,21 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 type SpyCountdownOperations struct {
@@ -45,6 +56,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"slices"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -37,4 +38,16 @@ func TestCountdown(t *testing.T) {
 			t.Errorf("got calls %v want %v", spySleepPrinter.Calls, want)
 		}
 	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
