5fbca6dbb3a89f429b4d390d06f0cc0ca5882342
Suraj Iyer <iyersuraj7@gmail.com>
2025-11-09 12:33:55 +0530

    added tests with dependency injection to ensure sleeper sleeps n times

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -6,13 +6,18 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := bytes.Buffer{}
-	Countdown(&buffer)
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := "3\n2\n1\nGo!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, got %d want %d", spySleeper.Calls, 3)
 	}
 }
