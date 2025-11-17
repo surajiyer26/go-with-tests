47975f701d33a0bf1944c7cb546d21e51b48a2e5
Suraj Iyer <iyersuraj7@gmail.com>
2025-11-17 23:11:49 +0530

    made use of select which helps us wait on multiple channels; added a test to know that urls taking more than 10s should return error

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	durationA := MeasureResponseTime(a)
-	durationB := MeasureResponseTime(b)
+var tenSecondTimeout = 10 * time.Second
 
-	if durationA < durationB {
-		return a
-	}
-
-	return b
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
-func MeasureResponseTime(url string) time.Duration {
-	startTime := time.Now()
-	http.Get(url)
-	return time.Since(startTime)
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
